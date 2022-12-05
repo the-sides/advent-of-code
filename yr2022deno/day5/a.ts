6ca190fe3fc4104cf8639713100356ada47de8ec
Jacob Sides <sides798@gmail.com>
2022-12-05 16:08:08 -0500

    Day 5: actually fun

---
+++ yr2022deno/day5/a.ts
@@ -0,0 +1,44 @@
+import { fromFileUrl } from "path";
+const input = await Deno.readTextFile(
+  fromFileUrl(import.meta.resolve("./input.txt")),
+);
+
+const lines = input.split("\r\n").filter((line) => "" !== line);
+const board = lines.filter((line) => !line.includes("move"));
+const moves = lines.filter((line) => line.includes("move"));
+const moveUnits : number[][] = moves.map((move) => [...move.matchAll(/\d+/g)]?.map(Number) ?? []);
+const lanes : string[][] = [...Array(9)].map(() => ([]))
+
+
+
+board.forEach((line) => {
+  [...line].forEach((char, i) => {
+    if (![" ", "[", "]"].includes(char) && !char.match(/\d+/g)?.length) {
+      const slot = (i + 3) / 4;
+      try {
+        // console.log('push', char, 'at', slot-1)
+        lanes[slot-1] = [char, ...lanes[slot-1]]
+      }
+      catch {
+        console.error(lanes, slot, char)
+      }
+    }
+  });
+});
+
+// console.log([...lanes])
+// lanes[0].pop()
+// console.log([...lanes])
+
+console.log('start', lanes)
+
+moveUnits.forEach(([qnt,from,to]) => {
+  // console.log(qnt,from,to)
+  for (let i = 0; i < qnt; i++) {
+    const last = lanes[from-1].pop() ?? '';
+    if(last !== '') lanes[to-1].push(last)
+    else console.error('value wasn\'t popped', qnt,from,to)
+  }
+})
+
+console.log(lanes.map(lane => lane.pop()).join(''));
