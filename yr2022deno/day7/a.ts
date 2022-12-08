d6b72b48e0f3eeee74bee1729badaa27f632a114
the-sides <sides798@gmail.com>
2022-12-07 22:47:56 -0500

    Day 7: i'm so sad, never worked :(((

--- yr2022deno/day7/a.ts
+++ yr2022deno/day7/a.ts
@@ -3,40 +3,100 @@ const input = await Deno.readTextFile(
   fromFileUrl(import.meta.resolve("./input.txt")),
 );
 
-interface file {
-  name: string;
-  size: number;
-}
-interface directory {
-  children: ;
-  size: number;
-}
-
-
-const tree: directory[];
-
-tree["/"]["brhvclj"] = {
-  "file": { size: 123 },
-  "dir1": {
-    children: {
-      "dir1a": { children: [], size: 1234 },
-      "dir1b": { children: [], size: 412 },
-    },
-  },
+const tree: any = {
+  '/': {
+    children: {},
+    size: 0,
+  }
 };
 
+// tree["/"].children["brhvclj"].children = {
+//   "file": { size: 123 },
+//   "dir1": {
+//     children: {
+//       "dir1a": { children: [], size: 1234 },
+//       "dir1b": { children: [], size: 412 },
+//     },
+//   },
+// };
+
 const cwd: string[] = [];
-const lines = input.split("\r\n");
+const lines = input.split("\n");
 // console.log(lines.length);
 
-const parseListOutput = (line: string) => {
-  // Do nothing?
-  const dirName = cwd.at(-1);
+const getCrntParent = () => {
+
+  if (cwd.length < 2) return;
+
+  const parent = cwd.reduce((parentDir, dir) => {
+    // console.log(dir,)
+    return parentDir?.[dir]?.children ?? parentDir;
+  }, tree);
+
+
+  if (!parent[cwd.at(-1)]) {
+    parent[cwd.at(-1)] = {
+      children: {},
+      size: 0
+    }
+  }
+
+  return parent;
+}
+
+const getCurrentContainer = () => {
+
+  return cwd.reduce((parentDir, dir) => {
+    if (!parentDir[dir].children) { throw new Error('lookup chain broke') }
+    return parentDir[dir].children;
+  }, tree);
+}
+
+const getCurrentParent = () => {
+
+  return cwd.reduce((parentDir, dir, ind) => {
+    if (!parentDir[dir].children) { throw new Error('lookup chain broke') }
+    return ind === cwd.length - 1 ? parentDir[dir] : parentDir[dir].children;
+  }, tree);
+}
+
+const foundDir = (newDirName) => {
+  // ['/']
+
+  const children = getCurrentContainer();
+
+  if (!children[newDirName]) {
+    children[newDirName] = {
+      children: {},
+      size: 0,
+    }
+  } else {
+    console.log('Already made', children)
+  }
+}
+
+// Will check for crnt and init if undefined
+const initCrntDir = () => getCrntParent();
+
+
+const trackFile = (line: string) => {
+  const crntDir = cwd.at(-1);
+  getCrntParent();
   const [a, b] = line.split(" ");
+
+  // File Types
+  //   Directory
   if (a === "dir") {
     // map it
-  } else {
+    foundDir(b)
+  }
+  // File
+  else {
     const size = Number(a);
+    getCurrentContainer()[b] = { size }
+
+    // Might have to be changed to consider sub directories. 
+    getCurrentParent().size += size;
   }
 };
 
@@ -48,7 +108,7 @@ const move = (dest: string) => {
   } else {
     cwd.push(dest);
   }
-  console.log("/" + cwd.join("/"));
+  console.log('/' + cwd.slice(1, cwd.length).join("/"));
 };
 
 const command = (line: string) => {
@@ -56,9 +116,8 @@ const command = (line: string) => {
     const dir = line.split("$ cd ")[1];
     move(dir);
   } else if (line.includes("$ ls")) {
-    // Populate tree
-    // Document items
-    // Track directories
+    // initDirectory()
+    // initCrntDir();
   } else {
     console.error("Unrecognized command", line);
     throw new Error("Unrecognized command");
@@ -66,7 +125,7 @@ const command = (line: string) => {
 };
 
 // Ignore the first cd /, assume we are already at root
-for (const line of lines.slice(1)) {
+for (const line of lines) {
   // Break command into types
   // Command
   if (line[0] === "$") {
@@ -76,5 +135,8 @@ for (const line of lines.slice(1)) {
     // only output is listing
     // look at current working directory
     // start defining tree
+    trackFile(line)
   }
 }
+
+console.log(tree['/'].children['lcz'])
