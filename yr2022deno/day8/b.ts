2466d901a508ea7164c88de9372b795d6ba65e03
Jacob Sides <sides798@gmail.com>
2022-12-09 00:30:10 -0500

    Day 8: part 2 we fall

--- yr2022deno/day8/b.ts
+++ yr2022deno/day8/b.ts
@@ -1,29 +1,128 @@
 import { fromFileUrl } from "path";
-const input = await Deno.readTextFile(
+const input = (await Deno.readTextFile(
   fromFileUrl(import.meta.resolve("./input.txt")),
-);
+)).replaceAll("\r", "");
 
-const frequency: any = {};
+const grid: number[][] = [];
+const scores: number[] = [];
+const guesses = [
+  13440,
+  99360,
+  292410,
+  // Ans Somewhere Here
+  148713264,
+  149577876,
+  155257920,
+];
 
-for (const char of input) {
-  console.log(char);
-  if (frequency[char]) {
-    frequency[char] += 1;
-  } else {
-    frequency[char] = 1;
+// Setup grid
+for (const [ind, line] of input.split("\n").entries()) {
+  console.log(line[0], ind);
+  grid[ind] = [];
+  for (const [charInd, char] of [...line].entries()) {
+    grid[ind][charInd] = Number(char);
   }
 }
 
-const windowSize = 14;
-for (let i = windowSize - 1; i < input.length; i++) {
-  // const char = input[i];
-  let a = i - (windowSize - 1);
-  let window = [...input].slice(a, i + 1);
-  if ([...new Set(window)].length !== windowSize) continue;
-  else {
-    console.log("index:", i, "letters processed:", i + 1);
-    break;
+const b = {
+  x: grid[0].length - 0,
+  y: grid.length - 0,
+};
+
+const checkToEdge = (cx: number, cy: number) => {
+  // All trees in one of 4 directions must be less than crnt to pass
+  const mustBeat = grid[cy][cx];
+  let x = 0, y = 0, treesVisible = 1, score = 1;
+  let visible = true;
+  const reset = () => {
+    treesVisible = 1;
+    visible = true;
+    x = cx;
+    y = cy;
+  };
+
+  // Up
+  reset();
+  while (--y >= 0) {
+    if (grid[y][x] >= mustBeat) {
+      visible = false;
+      break;
+    } else {
+      treesVisible++;
+    }
+  }
+  score *= treesVisible;
+  if (visible) {
+    scores.push(score);
+    return true;
+  }
+
+  // Down
+  // this one was off originally? why not right?
+  reset();
+  while (++y < b.y) {
+    if (grid[y][x] >= mustBeat) {
+      visible = false;
+      break;
+    } else {
+      treesVisible++;
+    }
+  }
+  score *= treesVisible;
+  if (visible) {
+    scores.push(score);
+    return true;
+  }
+
+  // Left
+  reset();
+  while (--x >= 0) {
+    if (grid[y][x] >= mustBeat) {
+      visible = false;
+      break;
+    } else {
+      treesVisible++;
+    }
+  }
+  score *= treesVisible;
+  if (visible) {
+    scores.push(score);
+    return true;
+  }
+
+  // Right
+  reset();
+  while (++x < b.x) {
+    if (grid[y][x] >= mustBeat) {
+      visible = false;
+      break;
+    } else {
+      treesVisible++;
+    }
+  }
+  score *= treesVisible;
+  if (visible) {
+    scores.push(score);
+    return true;
+  }
+
+  scores.push(score);
+  return false;
+};
+
+let visibleTrees = 0;
+for (let y = 0; y < grid.length; y++) {
+  const xRow = grid[y];
+  for (let x = 0; x < xRow.length; x++) {
+    checkToEdge(x, y) && visibleTrees++;
   }
 }
 
-// console.log(frequency)
+console.log(grid);
+const bestScore = [...new Set(scores)].sort((a, b) => b - a).at(0) ?? 0;
+console.log({ bestScore });
+if (guesses.includes(bestScore)) {
+  console.log("Which unfortunately has already been guessed");
+} else {
+  console.log("New Guess");
+}
