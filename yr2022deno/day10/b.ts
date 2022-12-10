f36f128740c4cea194027f12598c27d237878500
Jacob Sides <sides798@gmail.com>
2022-12-10 17:27:41 +0000

    Day 10: start pt2

--- yr2022deno/day10/b.ts
+++ yr2022deno/day10/b.ts
@@ -4,3 +4,33 @@ const input = (await Deno.readTextFile(
 )).replaceAll("\r", "");
 
 const lines = input.split("\n");
+
+let register = 1
+let ticks = 1;
+let sum = 0;
+
+const checkFor20th = () => {
+  if ((ticks + 20) % 40 === 0) {
+    sum += (register*ticks);
+    console.log(`${register} * ${ticks} = ${register*ticks} | sum: ${sum}`);
+  }
+}
+
+for (const cmd of lines) {
+  if (cmd === 'noop') {
+    ticks++
+    checkFor20th();
+    continue;
+  };
+
+  let [addx, unit] = cmd.split(' ');
+  if (addx !== 'addx') throw new Error('3rd command??');
+  unit = Number(unit)
+
+  ticks++;
+  checkFor20th();
+  ticks++;
+  register += unit;
+  checkFor20th();
+
+}
