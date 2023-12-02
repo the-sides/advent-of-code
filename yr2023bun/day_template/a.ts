87acbed877dc9e2dea334b0aeb67faf01cbf584d
the-sides <jacob.r.sides@gmail.com>
2023-12-02 02:21:07 -0500

    Year 2023 Template update to use attempt check

--- yr2023bun/day_template/a.ts
+++ yr2023bun/day_template/a.ts
@@ -2,14 +2,15 @@ const path = import.meta.dir + '/input.txt';
 const file = Bun.file(path);
 const input = await file.text();
 
-const actions: {[key: string]: () => void} = {
-    '<' : () => {},
-    '>' : () => {},
-    '^' : () => {},
-    'v' : () => {},
-};
+const sum = input.split('\n')
+    .reduce((accu: number, line: string) => {
+        return accu + line.length
+    }, 0);
 
-[...input]
-    .forEach((c:string) => {
 
-    });
+const attempts: any = ['here'];
+if (attempts.includes(sum)) {
+    console.log('You got old attempt', sum)
+}
+// Winner: 
+else console.log('New Sum:', sum)
